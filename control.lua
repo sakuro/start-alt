21682e05d64964c32a222693bfca1ee167db46ad
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2026-09-26 13:14:48 +0900

    :rotating_light: Allow writing LuaPlayer properties in luacheck

--- control.lua
+++ control.lua
@@ -1,6 +1,8 @@
 local enable_show_entity_info = function(event)
   local player = game.players[event.player_index]
-  player.game_view_settings.show_entity_info = true
+  -- LuaPlayer properties are writable at runtime; luacheck only sees a field of
+  -- the read-only global `game`.
+  player.game_view_settings.show_entity_info = true -- luacheck: ignore 122
 end
 
 script.on_event(defines.events.on_player_created, enable_show_entity_info)
